45c4fa1d8d15ae21355cea28c28983ffb77016a7
YaKozina <jupiteriangoose12@gmail.com>
2025-02-09 19:08:16 +0200

    calculating eps1 script

---
+++ 0nuSe82_CutsCalculating.cpp
@@ -0,0 +1,30 @@
+//calculating cuts for 0nuSe82
+#include "/pbs/home/y/ykozina/tutorial/MiModule/include/MiEvent.h"
+#include <iostream>
+R__LOAD_LIBRARY(/pbs/home/y/ykozina/tutorial/MiModule/lib/libMiModule.so);
+
+void Example_ROOT()
+{
+    TFile* f = new TFile("Default.root");  
+    TTree* s = (TTree*) f->Get("Event");  // Отримуємо дерево з іменем "Event"
+//  s->Print(); //get list of branches in Event
+
+int N = s->GetEntries(); //total number of entries
+int n_calos = 0; //number of calorimeter hits
+int passed1 = 0; //number of events which match the criteria n_calos = 2
+
+   MiEvent* Eve = new MiEvent();  
+   s->SetBranchAddress("Eventdata", &Eve);  // Прив'язуємо гілку до об'єкта Eve
+
+    for (UInt_t i = 0; i < N; i++) 
+  {		
+        s->GetEntry(i);  
+        n_calos = Eve->getCD()->getnoofcaloh(); //getnoofcaloh is a public method in MiCD.h to get the number of calorimeter hits
+        if (n_calos == 2) passed1++;
+
+    }	
+cout << endl << "EFFICIENCIES :" << endl;
+cout << "eps1 = " << (100.0 * passed1) / N << "% +- "<< (100.0 * sqrt(double(passed1)) ) / N << "%" << endl;
+
+}
+
