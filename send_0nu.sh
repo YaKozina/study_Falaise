808bc74bbf789d6a2256365354529ed6be91283c
YaKozina <jupiteriangoose12@gmail.com>
2025-03-15 18:10:40 +0200

    added time measuring

--- send_0nu.sh
+++ send_0nu.sh
@@ -3,16 +3,20 @@
 # SLURM options:
 
 #SBATCH --job-name=0nubb_Se82         	 # Job name
-#SBATCH --partition=flash              # Partition choice (most generally we work with htc, but for quick debugging you can use
-							         #					 #SBATCH --partition=flash. This avoids waiting times, but is limited to 1hr)
-#SBATCH --mem=8G                     	 # RAM
+#SBATCH --mem=5G                     	 # RAM
 #SBATCH --licenses=sps                   # When working on sps, must declare license!!
 
-#SBATCH --time=10:10                 	 # Time for the job in format “minutes:seconds” or  “hours:minutes:seconds”, “days-hours”
+#SBATCH --time=10:00                 	 # Time for the job in format “minutes:seconds” or  “hours:minutes:seconds”, “days-hours”
 #SBATCH --cpus-per-task=1                # Number of CPUs
-# #SBATCH --output=/pbs/home/y/ykozina/tutorial/0nu/$A_$a_out.log 
-# #SBATCH --error=/pbs/home/y/ykozina/tutorial/0nu/$A_$a_err.log
+
+start_time=$(date +%s)
 
 /sps/nemo/sw/redhat-9-x86_64/snsw/opt/falaise-5.1.2/bin/flsimulate -c Simu_0nu_Se82.conf -o Simu_0nu_Se82.brio
 #/sps/nemo/sw/redhat-9-x86_64/snsw/opt/falaise-5.1.2/bin/flreconstruct -p p_MiModule_v00.conf -i Simu_0nu_Se82.brio -o Reco_0nu_Se82.brio
+end_time=$(date +%s)
+
+runtime=$((end_time - start_time))
+
+echo "Job execution time: $runtime seconds"
+
 
