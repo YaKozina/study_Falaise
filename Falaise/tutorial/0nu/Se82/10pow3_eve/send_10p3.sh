5b0005d4e403a39b50c0ccf7f92462cb59163052
YaKozina <jupiteriangoose12@gmail.com>
2025-03-31 12:02:34 +0200

    scripts 10^3 events (failed)

---
+++ Falaise/tutorial/0nu/Se82/10pow3_eve/send_10p3.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# SLURM options:
+
+#SBATCH --job-name=0nubb_Se82_10p3eve         	 # Job name
+#SBATCH --mem=10G                     	 # RAM
+#SBATCH --licenses=sps                   # When working on sps, must declare license!!
+
+#SBATCH --time=20:00                 	 # Time for the job in format “minutes:seconds” or  “hours:minutes:seconds”, “days-hours”
+#SBATCH --cpus-per-task=1                # Number of CPUs
+
+start_time=$(date +%s)
+
+/sps/nemo/sw/redhat-9-x86_64/snsw/opt/falaise-5.1.2/bin/flsimulate -c Simu_0nu_Se82_10p3eve.conf -o Simu_0nu_Se82_10p3eve.brio
+#/sps/nemo/sw/redhat-9-x86_64/snsw/opt/falaise-5.1.2/bin/flreconstruct -p p_MiModule_v00.conf -i Simu_0nu_Se82.brio -o Reco_0nu_Se82.brio
+
+end_time=$(date +%s)
+
+runtime=$((end_time - start_time))
+
+echo "Job execution time: $runtime seconds"
+
+
