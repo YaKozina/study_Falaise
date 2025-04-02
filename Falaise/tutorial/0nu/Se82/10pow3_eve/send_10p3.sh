49d31598e6d836efd1ee56f0ba6cf6f835f6ccce
YaKozina <jupiteriangoose12@gmail.com>
2025-04-02 17:32:35 +0200

    send script draft

--- Falaise/tutorial/0nu/Se82/10pow3_eve/send_10p3.sh
+++ Falaise/tutorial/0nu/Se82/10pow3_eve/send_10p3.sh
@@ -9,10 +9,21 @@
 #SBATCH --time=20:00                 	 # Time for the job in format “minutes:seconds” or  “hours:minutes:seconds”, “days-hours”
 #SBATCH --cpus-per-task=1                # Number of CPUs
 
+
+
+source ${THRONG_DIR}/config/supernemo_profile.bash
+snswmgr_load_stack base@2024-09-04
+snswmgr_load_setup falaise@5.1.2
+
+
 start_time=$(date +%s)
 
 /sps/nemo/sw/redhat-9-x86_64/snsw/opt/falaise-5.1.2/bin/flsimulate -c Simu_0nu_Se82_10p3eve.conf -o Simu_0nu_Se82_10p3eve.brio
-#/sps/nemo/sw/redhat-9-x86_64/snsw/opt/falaise-5.1.2/bin/flreconstruct -p p_MiModule_v00.conf -i Simu_0nu_Se82.brio -o Reco_0nu_Se82.brio
+
+CONF_FAL=/sps/nemo/sw/Falaise/install_develop/share
+CONF_FAL=$CONF_FAL/Falaise-4.1.0/resources/snemo/demonstrator/reconstruction/
+
+/sps/nemo/sw/redhat-9-x86_64/snsw/opt/falaise-5.1.2/bin/flreconstruct -i Simu_0nu_Se82_10p3eve.brio -p $CONF_FAL/official-2.0.0.conf -o Reco_0nu_Se82_10p3eve.brio 
 
 end_time=$(date +%s)
 
